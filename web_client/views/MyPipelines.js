38f37e0c0b2fcd6faf020883e3a737bc95e87f8c
Lucas Thevenard <thevenard.lucas@gmail.com>
2018-05-25 10:18:58 +0200

    upload resultats

--- web_client/views/MyPipelines.js
+++ web_client/views/MyPipelines.js
@@ -30,11 +30,6 @@ var MyPipelines = View.extend({
     this.collection = new PipelineCollection;
     this.statusKeys = getStatusKeys();
 
-    this.carmin.listPipelines().then(function (data) {
-      console.log(data);
-    });
-
-    return ;
 
     // Get all pipeline executions
     restRequest({
@@ -71,18 +66,17 @@ var MyPipelines = View.extend({
     return new Promise(function (resolve) {
       _.each(pipeline_executions, function(execution) {
         if (execution.status != this.statusKeys[4] && execution.status != this.statusKeys[5]) {
-          this.carmin.getExecution(execution.vipExecutionId, function (workflow) {
-            // Update the status
+          this.carmin.getExecution(execution.vipExecutionId).then(function (workflow) {
+
             if (execution.status != workflow.status.toUpperCase()) {
               restRequest({
                 method: 'PUT',
                 url: 'pipeline_execution/' + execution._id + "/status",
-                data: {
-                  'status': workflow.status.toUpperCase()
-                }
+                data: { 'status': workflow.status.toUpperCase() }
               });
             }
-          }.bind(this));
+
+          });
         }
       }.bind(this));
       resolve();
@@ -92,7 +86,8 @@ var MyPipelines = View.extend({
   getResults: function (pipeline_executions) {
     _.each(pipeline_executions, function (execution) {
       if (execution.status != this.statusKeys[4] && execution.status != this.statusKeys[5]) {
-        this.carmin.getExecutionResults(execution.get('vipExecutionId'), function (results) {
+
+        this.carmin.getExecutionResults(execution.get('vipExecutionId')).then(function (results){
           _.each(results, function (result) {
 
             // SCRIPT AVANT CHANGEMENT DE L'API VIP
@@ -101,11 +96,15 @@ var MyPipelines = View.extend({
             result = result.replace('?action=list', '');
             result = result.replace('http://vip.creatis.insa-lyon.fr/rest/path/', '');
 
-            this.carmin.getFolderDetails(result, function (data) {
+            this.carmin.getFolderDetails(result).then(function (data) {
+
+                var i = 0;
+                this.downloadFileFromVip(execution, data, i);
+
               _.each(data, function (res) {
 
 
-                /*this.carmin.downloadFile(res.path, function (content) {
+                /*this.carmin.downloadFile(res.path).then(function (content) {
                   var fileName = res.path.substring(res.path.lastIndexOf('/') + 1);
                   var data = content;
                   var myFile = new File([data], fileName, {type:"application/octet-stream"});
@@ -134,36 +133,50 @@ var MyPipelines = View.extend({
               }.bind(this));
             }.bind(this));
 
-            // SCRIPT APRES CHANGEMENT DE L'API VIP
 
+            // SCRIPT APRES CHANGEMENT DE L'API VIP
+            // ........
 
           }.bind(this));
         }.bind(this));
       }
     }.bind(this));
+  },
 
-    /*this.carmin.getExecutionResults(workflow.identifier, function (data) {
-      _.each(data, function (result) {
+  downloadFileFromVip: function (execution, results, i) {
+    //console.log(i);
+    if (i == results.length)
+      return ;
 
-        // SCRIPT AVANT LE CHANGEMENT DE L'API VIP
-        result = result.replace('/output.txt', '');
-        result = result.replace('content', 'list');
-        result = result.replace('?action=list', '');
-        result = result.replace('http://vip.creatis.insa-lyon.fr/rest/path/', '');
+    var res = results[i].path;
+
+    this.carmin.downloadFile(res).then(function (content) {
+      var fileName = res.substring(res.lastIndexOf('/') + 1);
+      var myFile = new File([content], fileName, {type:"application/octet-stream"});
+      var size = myFile.size;
+
+      console.log(content);
+      console.log(myFile);
+      console.log(size);
 
 
-        this.carmin.getFolderDetails(result, function (data) {
-          _.each(data, function (res) {
-            this.carmin.downloadFile(res.path, function (content) {
-              console.log(content);
-            });
-          }.bind(this));
-        }.bind(this));
+      restRequest({
+        method: 'POST',
+        url: 'file?parentType=folder&parentId=' + execution.get('pathResultGirder') + '&name=' + fileName
+        + '&size=' + size,
+        data: myFile,
+        cache: false,
+        contentType: false,
+        processData: false,
+        encoding: null
+      }).done((resp) => {
+        //console.log(resp);
+      });
+      
+      i++;
+      this.downloadFileFromVip(execution, results, i);
 
-        // SCRIPT NON TESTE APRES LE CHANGEMENT DE L'API VIP
-
-      }.bind(this));
-    }.bind(this));*/
+    }.bind(this));
   }
 
 });
