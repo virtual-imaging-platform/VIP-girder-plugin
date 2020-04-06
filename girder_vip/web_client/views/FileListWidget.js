e6a0bb9593fdadd3e43ce67475abc8d7bd38ba17
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2020-04-06 19:22:18 +0200

    forgot a file

---
+++ girder_vip/web_client/views/FileListWidget.js
@@ -0,0 +1,34 @@
+// Import utilities
+import { wrap } from '@girder/core/utilities/PluginUtils';
+import { AccessType } from '@girder/core/constants';
+import { hasTheVipApiKeyConfigured, isPluginActivatedOn } from '../utilities';
+
+// Import views
+import FileListWidget from '@girder/core/views/widget/FileListWidget';
+
+// Import about Creatis
+import ButtonListPipeline from '../templates/buttonListPipeline.pug';
+
+// Add an entry to the ItemView
+wrap(FileListWidget, 'render', function(render) {
+  render.call(this);
+
+  var item = this.parentItem;
+  if (! hasTheVipApiKeyConfigured() || ! isPluginActivatedOn(item)) {
+    return this;
+  }
+
+  if (item.get('_accessLevel') >= AccessType.READ) {
+    this.collection.each(file => {
+      this.$('li.g-file-list-entry.g-show-info[file-cid=' + file.cid']')
+        .after(ButtonListPipeline({file: file})));
+    });
+  }
+  return this;
+});
+
+FileListWidget.prototype.events['click a.vip-launch-pipeline'] = function (e) {
+    var cid = $(e.currentTarget).parent().attr('file-cid');
+    var fileId = this.collection.get(cid).id;
+    router.navigate("#file/" + fileId + "/#pipelines", {trigger: true});
+};
