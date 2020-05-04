7606529fe599ae0c646f20fd91c766630596733e
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2020-05-04 11:59:02 +0200

    integrate a file list in the file browser

---
+++ girder_vip/web_client/views/FileSelector.js
@@ -0,0 +1,113 @@
+// Import utilities
+import _ from 'underscore';
+import events from '@girder/core/events';
+import { wrap } from '@girder/core/utilities/PluginUtils';
+import CollectionCollection from '@girder/core/collections/CollectionCollection';
+import FileCollection from '@girder/core/collections/FileCollection';
+
+// Import views
+import View from '@girder/core/views/View';
+import HierarchyWidget from '@girder/core/views/widgets/HierarchyWidget';
+import FileListWidget from '@girder/core/views/widgets/FileListWidget';
+import BrowserWidget from '@girder/core/views/widgets/BrowserWidget';
+
+// import template
+import FileSelectorTemplate from '../templates/fileSelector.pug';
+
+// extends girder browser widget to :
+// - only show configured collections
+// - add an additionnal file selection step if the selected item has more than 1
+
+var FileSelector = BrowserWidget.extend({
+  // override initialise
+  initialize: function (settings) {
+
+    // only show configured collections
+    var filteredCollections = new CollectionCollection();
+    filteredCollections.filterFunc = c => c.name === 'Collection1';
+    var rootSelectorSettings = {
+      display: ['VIP Authorized Collections'],
+      groups: {'VIP Authorized Collections' : filteredCollections}
+    };
+
+    // use girder browser in item mode
+    BrowserWidget.prototype.initialize.call(this, {
+        showItems: true,
+        selectItem: true,
+        removeItemInfo: true,
+        rootSelectorSettings: rootSelectorSettings
+    });
+  },
+
+  // override render
+  render: function () {
+    BrowserWidget.prototype.render.call(this);
+
+    this.$('.modal-body.g-browser-widget').after(
+      FileSelectorTemplate().addClass('hidden')
+    );
+
+    return this;
+  },
+
+  toggleFileSelector: function() {
+    this.$('.modal-body.g-browser-widget').toggleClass('hidden');
+    this.$('.modal-body.vip-file-selector').toggleClass('hidden');
+  },
+
+  // override internal validate
+  _validate: function () {
+    // fetch item files
+    var item = this.selectedModel();
+    if ( ! item) {
+      // use girder browser error display
+      this.validate = () => Promise.reject("Please select an item");
+      BrowserWidget.prototype._validate.call(this);
+      return;
+    }
+
+    // fetch the item files to see how many there are
+    // use the fileListWidget as it fetch the file and we will show it if needed
+    this.fileListWidget = new FileListWidget({
+      itemId: item.id,
+      parentView: this,
+      el: this.$('.vip-file-selector .file-list'),
+    });
+    fileListWidget.on('g:changed', () => this.onFileListReady() );
+  },
+
+  // file list widget ready (item files received during validation process)
+  onFileListReady: function () {
+    var itemFiles = this.fileListWidget.collection;
+    if (itemFiles.length === 0) {
+      // use girder browser error display
+      this.validate = () => Promise.reject("This item does not have any file");
+      BrowserWidget.prototype._validate.call(this);
+    } else if (this.itemFiles.length === 1) {
+      // it's ok
+      this.validate = () => Promise.resolve();
+      BrowserWidget.prototype._validate.call(this);
+    } else {
+      // there are several files, show the FileListWidget
+      this.toggleFileSelector();
+    }
+
+  };
+
+});
+
+// remove the download and view (and rocket link) next to the items
+// (only on this file selection use)
+import HierarchyWidget from '@girder/core/views/widgets/HierarchyWidget';
+wrap(HierarchyWidget, 'initialize', function(initialize, settings) {
+  settings = settings || {};
+  if (this.parentView instanceof FileSelector) {
+    settings.downloadLinks = false,
+    settings.viewLinks = false,
+    this.viewVipRocket = false,
+  }
+  // Call the parent init
+  initialize.call(this, settings);
+});
+
+export default FileSelector;
--- girder_vip/web_client/views/LaunchVipPipeline.js
+++ girder_vip/web_client/views/LaunchVipPipeline.js
@@ -1,35 +1,18 @@
 // Import utilities
 import events from '@girder/core/events';
-import { wrap } from '@girder/core/utilities/PluginUtils';
-import CollectionCollection from '@girder/core/collections/CollectionCollection';
 
 // Import views
 import View from '@girder/core/views/View';
-import HierarchyWidget from '@girder/core/views/widgets/HierarchyWidget';
-import FileSelectorModal from './FileSelectorModal';
-import BrowserWidget from '@girder/core/views/widgets/BrowserWidget';
+import FileSelector from './FileSelector';
 
 // Import templates
 import LaunchTemplate from '../templates/launchVipPipeline.pug';
 
 var LaunchVipPipeline = View.extend({
-    initialize: function (settings) {
-
-    var filteredCollections = new CollectionCollection();
-    filteredCollections.filterFunc = c => c.name === 'Collection1';
-    var rootSelectorSettings = {
-      display: ['VIP Authorized Collections'],
-      groups: {'VIP Authorized Collections' : filteredCollections}
-    };
-
-    // todo filters collections for root selector
-    this._browserWidgetView = new BrowserWidget({
-          parentView: this,
-          showItems: true,
-          selectItem: true,
-          removeItemInfo: true,
-          rootSelectorSettings: rootSelectorSettings
-      });
+  initialize: function (settings) {
+    this._fileSelector = new FileSelector({
+      parentView: this
+    })
     this.render();
   },
 
@@ -37,7 +20,7 @@ var LaunchVipPipeline = View.extend({
     'click .select-girder-file' : 'chooseFile'
   },
 
-  render: function (executions) {
+  render: function () {
     // Display the list of executions
     this.$el.html( LaunchTemplate() );
 
@@ -46,54 +29,13 @@ var LaunchVipPipeline = View.extend({
 
   // Delete a executon of the db
   chooseFile: function (e) {
-      this._browserWidgetView.setElement($('#g-dialog-container')).render();
+      this._fileSelector.setElement($('#g-dialog-container')).render();
   }
 
 }, {
     fetchAndInit: function (application, version) {
       events.trigger('g:navigateTo', LaunchVipPipeline);
     }
-});
-
-wrap(BrowserWidget, 'initialize', function(initialize, settings) {
-  settings = settings || {};
-  this.removeItemInfo = settings.removeItemInfo || false;
-
-  // Call the parent init
-  initialize.call(this, settings);
-});
-
-// replace to remove _downloadLinks, _viewLinks and _showSizes from the
-// hierarchy widget
-wrap(BrowserWidget, '_renderHierarchyView', function(_renderHierarchyView) {
-  if (this._hierarchyView) {
-      this.stopListening(this._hierarchyView);
-      this._hierarchyView.off();
-      this.$('.g-hierarchy-widget-container').empty();
-  }
-  if (!this.root) {
-      return;
-  }
-  this.$('.g-wait-for-root').removeClass('hidden');
-  this._hierarchyView = new HierarchyWidget({
-      el: this.$('.g-hierarchy-widget-container'),
-      parentView: this,
-      parentModel: this.root,
-      checkboxes: false,
-      routing: false,
-      showActions: false,
-      showItems: this.showItems,
-      downloadLinks: ! this.removeItemInfo,
-      viewLinks: ! this.removeItemInfo,
-      showSizes: ! this.removeItemInfo,
-      onItemClick: _.bind(this._selectItem, this),
-      defaultSelectedResource: this.defaultSelectedResource,
-      highlightItem: this.highlightItem,
-      paginated: this.paginated,
-      showMetadata: this.showMetadata
-  });
-  this.listenTo(this._hierarchyView, 'g:setCurrentModel', this._selectModel);
-  this._selectModel();
 });
 
 export default LaunchVipPipeline;
