e0d26f89a1c9888aa59ef3911b09b913813e5c6d
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2022-06-23 11:43:42 +0200

    include compare versions inside plugin

--- girder_vip/web_client/utilities/vipPluginUtils.js
+++ girder_vip/web_client/utilities/vipPluginUtils.js
@@ -2,7 +2,6 @@
 import _ from 'underscore';
 import { getCurrentUser } from '@girder/core/auth';
 import { restRequest, cancelRestRequests } from '@girder/core/rest';
-import compareVersions from 'compare-versions';
 import events from '@girder/core/events';
 import FolderModel from '@girder/core/models/FolderModel';
 import ApiKeyCollection from '@girder/core/collections/ApiKeyCollection.js'
@@ -446,6 +445,91 @@ function sortPipelines(allPipelines) {
 }
 
 
+// compare versions : from https://github.com/omichelsen/compare-versions
+
+// The MIT License (MIT)
+
+// Copyright (c) 2015-2021 Ole Michelsen
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+var semver = /^v?(?:\d+)(\.(?:[x*]|\d+)(\.(?:[x*]|\d+)(\.(?:[x*]|\d+))?(?:-[\da-z\-]+(?:\.[\da-z\-]+)*)?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?)?)?$/i;
+
+function indexOrEnd(str, q) {
+  return str.indexOf(q) === -1 ? str.length : str.indexOf(q);
+}
+
+function split(v) {
+  var c = v.replace(/^v/, '').replace(/\+.*$/, '');
+  var patchIndex = indexOrEnd(c, '-');
+  var arr = c.substring(0, patchIndex).split('.');
+  arr.push(c.substring(patchIndex + 1));
+  return arr;
+}
+
+function tryParse(v) {
+  return isNaN(Number(v)) ? v : Number(v);
+}
+
+function validate(version) {
+  if (typeof version !== 'string') {
+    throw new TypeError('Invalid argument expected string');
+  }
+  if (!semver.test(version)) {
+    throw new Error('Invalid argument not valid semver');
+  }
+}
+
+return function compareVersions(v1, v2) {
+  [v1, v2].forEach(validate);
+
+  var s1 = split(v1);
+  var s2 = split(v2);
+
+  for (var i = 0; i < Math.max(s1.length - 1, s2.length - 1); i++) {
+    var n1 = parseInt(s1[i] || 0, 10);
+    var n2 = parseInt(s2[i] || 0, 10);
+
+    if (n1 > n2) return 1;
+    if (n2 > n1) return -1;
+  }
+
+  var sp1 = s1[s1.length - 1];
+  var sp2 = s2[s2.length - 1];
+
+  if (sp1 && sp2) {
+    var p1 = sp1.split('.').map(tryParse);
+    var p2 = sp2.split('.').map(tryParse);
+
+    for (i = 0; i < Math.max(p1.length, p2.length); i++) {
+      if (p1[i] === undefined || typeof p2[i] === 'string' && typeof p1[i] === 'number') return -1;
+      if (p2[i] === undefined || typeof p1[i] === 'string' && typeof p2[i] === 'number') return 1;
+
+      if (p1[i] > p2[i]) return 1;
+      if (p2[i] > p1[i]) return -1;
+    }
+  } else if (sp1 || sp2) {
+    return sp1 ? -1 : 1;
+  }
+
+  return 0;
+};
 
 export {
   messageGirder,
