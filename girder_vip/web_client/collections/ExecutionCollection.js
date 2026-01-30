a819b9314a1f84250fcc764c5e9a39229392c092
Guillaume V. <guillaume.vinet@creatis.insa-lyon.fr>
2026-01-30 15:02:20 +0100

    Fix [Vip exec history] : Add vip executions filtering by current user id

--- girder_vip/web_client/collections/ExecutionCollection.js
+++ girder_vip/web_client/collections/ExecutionCollection.js
@@ -1,5 +1,6 @@
 // Import utilities
 import { SORT_DESC } from '@girder/core/constants';
+import { getCurrentUser } from '@girder/core/auth';
 
 import Collection from '@girder/core/collections/Collection';
 
@@ -9,7 +10,19 @@ var ExecutionCollection = Collection.extend({
     resourceName: 'vip_execution',
     model: ExecutionModel,
     sortField: 'timestampCreation',
-    sortDir: SORT_DESC
+    sortDir: SORT_DESC,
+
+    fetch: function(options = {}){
+        const fetchPromise = Collection.prototype.fetch.call(this, options);
+        // Filter the executions by userId
+        fetchPromise.then(() => {
+            const currentUserId = getCurrentUser().id;
+            const filteredExecs = this.filter(execution => execution.get('userId') === currentUserId);
+            this.reset(filteredExecs);
+        });
+
+        return fetchPromise;
+    }
 });
 
 export default ExecutionCollection;
